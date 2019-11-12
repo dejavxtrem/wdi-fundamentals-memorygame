9d3830b6564f523ef0739bf5321b2a4a350df825
dejavxtrem <dejavxtrem@hotmail.com>
2019-11-12 16:15:30 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+//console.log("up and running");
+//queen card
+const cardOne = "queen";
+const cardTwo = "queen";
+
+//Kings card
+const cardThree = "king";
+const cardFOur = "king";
+
+console.log(`User flipped : ${cardTwo}`);
+console.log(`User flipped : ${cardOne}`);
