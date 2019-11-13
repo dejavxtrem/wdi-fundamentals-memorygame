46a21e2baac2e6ddab77a8f4c66c5047a44e0a7b
dejavxtrem <dejavxtrem@hotmail.com>
2019-11-13 13:16:39 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,17 +13,31 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = []
 
-const cardOne = cards[0];
+/*const cardOne = cards[0];
 cardsInPlay.push(cardOne);
 
 console.log(`User flipped: ${cardOne}`);
 const cardTwo = cards[2];
 
 cardsInPlay.push(cardTwo);
-console.log(`User flipped: ${cardTwo}`);
+console.log(`User flipped: ${cardTwo}`);*/
 
-if (cardsInPlay.length === 2) {
+let checkForMatch = () => {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match");
-    } else {alert("Sorry, try again")};
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 }
+
+let flipCard = (cardId) => {
+    console.log(`User flipped: ${cards[cardId]}`);
+    cardsInPlay.push(cards[cardId]);
+
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+}
+
+flipCard(0);
+flipCard(2);
