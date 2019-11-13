5020bd11662de0b3bb3368da448eec77fb914a64
dejavxtrem <dejavxtrem@hotmail.com>
2019-11-12 21:27:00 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
 //console.log("up and running");
 //queen card
-const cardOne = "queen";
-const cardTwo = "queen";
+//const cardOne = "queen";
+//const cardTwo = "queen";
 
 //Kings card
-const cardThree = "king";
-const cardFOur = "king";
+//const cardThree = "king";
+//const cardFOur = "king";
 
-console.log(`User flipped : ${cardTwo}`);
-console.log(`User flipped : ${cardOne}`);
+//console.log(`User flipped : ${cardTwo}`);
+//console.log(`User flipped : ${cardOne}`);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = []
+
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log(`User flipped: ${cardOne}`);
+const cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+console.log(`User flipped: ${cardTwo}`);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match");
+    } else {alert("Sorry, try again")};
+}
